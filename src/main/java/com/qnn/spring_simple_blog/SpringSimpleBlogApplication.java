fe9362409400d5d097a43f9ee070baaa3b022d90
QnHyn <18855953229@163.com>
2019-11-17 20:31:18 +0800

    springboot 1.2.5 后台项目初始化

---
+++ src/main/java/com/qnn/spring_simple_blog/SpringSimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.qnn.spring_simple_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSimpleBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qnn/spring_simple_blog/SpringSimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnn.spring_simple_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
